611bd96f1e28ad7d3ff4bf4dc83222e18a419c02
diegobejar1011 <diegobejar2004@gmail.com>
2024-02-14 06:41:38 -0600

    Prueba1

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
 import * as crypto from "crypto";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
@@ -31,67 +31,64 @@ const verify_signature_middleware = (
     next();
   } catch (error) {
     res.status(400).json({
-        success: false,
-        message: 'No autorizado'
-    })
+      success: false,
+      message: "No autorizado",
+    });
   }
 };
 
 const verify_signature = (req: Request) => {
-    try {
-        const signature = crypto
-            .createHmac("sha256", "1234")
-            .update(JSON.stringify(req.body))
-            .digest("hex");
-        const trusted = Buffer.from(`sha256=${signature}`, "ascii");
-        const untrusted = Buffer.from(
-            req.header("x-hub-signature-256") ?? "",
-            "ascii"
-        );
-        return crypto.timingSafeEqual(trusted, untrusted);
-    } catch (error: any) {
-        throw new Error(error)
-    }
-}
+  try {
+    const signature = crypto
+      .createHmac("sha256", "1234")
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+    const trusted = Buffer.from(`sha256=${signature}`, "ascii");
+    const untrusted = Buffer.from(
+      req.header("x-hub-signature-256") ?? "",
+      "ascii"
+    );
+    return crypto.timingSafeEqual(trusted, untrusted);
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
 
 //notificación de discord
 
-app.post(
-  "/github-event",
-  (req: Request, res: Response) => {
-    try {
-      const { body } = req;
-      const { action, sender, repository } = body;
-      const event = req.headers["x-github-event"];
-      console.log(
-        `Received event ${event} from ${sender.login} for repository ${repository.name}`
-      );
-      let message = "";
-      switch (event) {
-        case "issues":
-          message = `Action: ${action}`;
-          break;
-        case "push":
-          message = `Commits: ${body.commits.length}`;
-          break;
-        case "star":
-          message = `Starred by ${sender.login}`;
-          break;
-        default:
-          message = "Event not handled";
-      }
-
-      notifyDiscord(message);
-
-      res.status(200).json({
-        success: true,
-      });
-
-    } catch (error: any) {
-        res.status(500).send(error.message);
+app.post("/github-event", (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const { action, sender, repository } = body;
+    const event = req.headers["x-github-event"];
+    console.log(
+      `Received event ${event} from ${sender.login} for repository ${repository.name}`
+    );
+    let message = "";
+    switch (event) {
+      case "issues":
+        const { issue } = req.body;
+        message = `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`;
+        break;
+      case "push":
+        message = `${sender.login} pushes on ${repository.full_name}`;
+        break;
+      case "star":
+        message = `Starred by ${sender.login}`;
+        break;
+      default:
+        message = "Event not handled";
     }
+
+    notifyDiscord(message);
+
+    res.status(200).json({
+      success: true,
+    });
+  } catch (error: any) {
+    res.status(500).send(error.message);
   }
-);
+});
 
 const webhookURL =
   "https://discord.com/api/webhooks/1207294858436411454/g1C1hBnrofo9nwzcWyGT_iB-CCG1_NedMDCCGfOOgfkqqGaowwxKp0IZpJ4cr5LGk5fE";
