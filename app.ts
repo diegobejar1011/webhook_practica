9940b67b655a87790d025f61417fdb0bf50d3885
diegobejar1011 <diegobejar2004@gmail.com>
2024-02-14 06:35:39 -0600

    Cambios realizados

--- app.ts
+++ app.ts
@@ -1,73 +1,115 @@
-import express, { Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import * as crypto from "crypto";
+import cors from "cors"
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) =>{
-    res.status(200).json({ success: true});
+app.get("/", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ success: true });
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is running at 3000');
+app.listen(3000, () => {
+  console.log("Server is running at 3000");
 });
-
 
 //middleware
 
-const verify_signature_middleware = (req: Request, res:Response, next: NextFunction) => {
-    if(!req) {
-        res.status(401).json({
-            success: false,
-            message: "Sin autorizacion"
-        })
-    }
+const verify_signature_middleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    verify_signature(req);
     next();
+  } catch (error) {
+    res.status(400).json({
+        success: false,
+        message: 'No autorizado'
+    })
+  }
+};
+
+const verify_signature = (req: Request) => {
+    try {
+        const signature = crypto
+            .createHmac("sha256", "1234")
+            .update(JSON.stringify(req.body))
+            .digest("hex");
+        const trusted = Buffer.from(`sha256=${signature}`, "ascii");
+        const untrusted = Buffer.from(
+            req.header("x-hub-signature-256") ?? "",
+            "ascii"
+        );
+        return crypto.timingSafeEqual(trusted, untrusted);
+    } catch (error: any) {
+        throw new Error(error)
+    }
 }
 
-app.post("/github-event", verify_signature_middleware, (req: Request, res: Response) => {
-    const { body } = req;
-    const { action, sender, repository } = body;
-    const event = req.headers['x-github-event'];
-    console.log(`Received event ${event} from ${sender.login} for repository ${repository.name}`);
-    let message = "";
-    switch (event) {
+//notificación de discord
+
+app.post(
+  "/github-event",
+  (req: Request, res: Response) => {
+    try {
+      const { body } = req;
+      const { action, sender, repository } = body;
+      const event = req.headers["x-github-event"];
+      console.log(
+        `Received event ${event} from ${sender.login} for repository ${repository.name}`
+      );
+      let message = "";
+      switch (event) {
         case "issues":
-            message = `Action: ${action}`;
-            break;
-        case "push": 
-            message = (`Commits: ${body.commits.length}`);
-            break;
+          message = `Action: ${action}`;
+          break;
+        case "push":
+          message = `Commits: ${body.commits.length}`;
+          break;
         case "star":
-            message = (`Starred by ${sender.login}`);
-            break;
+          message = `Starred by ${sender.login}`;
+          break;
         default:
-            message = ("Event not handled");
+          message = "Event not handled";
+      }
+
+      notifyDiscord(message);
+
+      res.status(200).json({
+        success: true,
+      });
+
+    } catch (error: any) {
+        res.status(500).send(error.message);
     }
+  }
+);
 
-    notifyDiscord(message);
-    res.status(200).json({
-        success: true
-    })
-});
-
-const webhookURL = "https://discord.com/api/webhooks/1207294858436411454/g1C1hBnrofo9nwzcWyGT_iB-CCG1_NedMDCCGfOOgfkqqGaowwxKp0IZpJ4cr5LGk5fE";
+const webhookURL =
+  "https://discord.com/api/webhooks/1207294858436411454/g1C1hBnrofo9nwzcWyGT_iB-CCG1_NedMDCCGfOOgfkqqGaowwxKp0IZpJ4cr5LGk5fE";
 const notifyDiscord = async (message: string) => {
+  const body = {
+    content: message,
+  };
 
-    const body = {
-        content: message
-    }
+  const response = await fetch(webhookURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(body),
+  });
 
-    const response = await fetch(webhookURL, {
-        method: "POST",
-        headers: { "content-type": "application/json"},
-        body: JSON.stringify(body)
-    })
+  if (!response.ok) {
+    console.log("error al enviar el mensaje");
+    return false;
+  }
 
-    if(!response.ok) {
-        console.log("error al enviar el mensaje")
-        return false;
-    }
-
-    return true;
-}
+  return true;
+};
